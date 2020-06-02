6174b294c359f2c61f1b5bdf650c0c713c2f2696
octobrekids <chnrd.k@gmail.com>
2020-06-02 23:44:31 +0700

    03-asyncawait

---
+++ 03-AsyncAwait/start.js
@@ -0,0 +1,71 @@
+(() => {
+    //  how asynchronous code works in Javascript
+    // function simulateAsyncAPI(text,  timeout) {
+    //     setTimeout(() => console.log(text), timeout);
+    // }
+
+    // simulateAsyncAPI('A', 1000);
+    // simulateAsyncAPI('B', 500);
+    // simulateAsyncAPI('C', 100);
+
+    // callback
+    // function simulateAsyncAPI(text, timeout, callback) {
+    //     setTimeout(() => {
+    //         console.log(text)   
+    //         if (callback){
+    //             callback();
+    //         }
+    //     }, timeout);
+    // }
+
+    // callback hell
+    // simulateAsyncAPI('A', 1000, () => {
+    //     simulateAsyncAPI('B', 500,  () => {
+    //         simulateAsyncAPI('C', 100);
+    //     });
+    // });
+
+    // promise ->  solve callback hell  problem
+    // function simulateAsyncAPI(text, timeout) {
+    //     return new Promise((resolve, reject) => {
+    //         setTimeout(() =>  {
+    //             if  (text === 'B') return reject('B got rejected');
+    //             console.log(text);
+    //             resolve();
+    //         }, timeout);
+    //     })
+    // }
+
+    // simulateAsyncAPI('A', 1000)
+    // .then(() => {
+    //    return  simulateAsyncAPI('B', 500);
+    // })
+    // .then(() =>  {
+    //     return  simulateAsyncAPI('C', 100);
+    // })
+    // .catch((error) =>  {
+    //     console.error(error);
+    // })
+
+    // async await
+    function simulateAsyncAPI(text, timeout) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() =>  {
+                if  (text === 'B') return reject('B got rejected');
+                console.log(text);
+                resolve();
+            }, timeout);
+        })
+    }
+
+    async function run() {
+        try  {
+            await simulateAsyncAPI('A', 1000);
+            await simulateAsyncAPI('B', 500);
+            await simulateAsyncAPI('C', 100);
+        } catch (error)  {
+            console.error(error);
+        }
+    }
+    run();
+})();
